60241dd698bfd91b1e3e072eecec43f51ee918d4
Ashish Singh <ashishcodes4@gmail.com>
2018-12-24 01:07:28 +0530

    Updates

---
+++ precache-manifest.2e59f7521e4704a21d789cf923568b88.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "eee007e077aef4e0d7e6ee60b7013c47",
+    "url": "/ashishcodes4.github.io/static/media/dom.eee007e0.jpeg"
+  },
+  {
+    "revision": "35c87b78f187ea6a9f41",
+    "url": "/ashishcodes4.github.io/static/css/main.a74ccddb.chunk.css"
+  },
+  {
+    "revision": "861df9171b57cbc4f000",
+    "url": "/ashishcodes4.github.io/static/js/1.861df917.chunk.js"
+  },
+  {
+    "revision": "db2bf116c5a8ea9041b5",
+    "url": "/ashishcodes4.github.io/static/js/runtime~main.db2bf116.js"
+  },
+  {
+    "revision": "40dde8ba1e578ed6ade0f4eb80e44fef",
+    "url": "/ashishcodes4.github.io/static/media/canvas.40dde8ba.jpeg"
+  },
+  {
+    "revision": "35c87b78f187ea6a9f41",
+    "url": "/ashishcodes4.github.io/static/js/main.35c87b78.chunk.js"
+  },
+  {
+    "revision": "445d8c01f8c1d7fd05a30cce9935bbf1",
+    "url": "/ashishcodes4.github.io/static/media/github.445d8c01.jpg"
+  },
+  {
+    "revision": "bcbd9ebc6a3c9d68dbbd9055c06518e1",
+    "url": "/ashishcodes4.github.io/static/media/reactFir.bcbd9ebc.jpeg"
+  },
+  {
+    "revision": "a67b0d1dbf86d27474ac0b10ccaef958",
+    "url": "/ashishcodes4.github.io/static/media/reactHer.a67b0d1d.jpeg"
+  },
+  {
+    "revision": "86e77555c0d073a805c52045f9a67896",
+    "url": "/ashishcodes4.github.io/static/media/avatar.86e77555.jpeg"
+  },
+  {
+    "revision": "f74d6bb12e4fb9897919a5591b1940f3",
+    "url": "/ashishcodes4.github.io/index.html"
+  }
+];
